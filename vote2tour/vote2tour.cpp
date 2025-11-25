833bb57f8199081de3abe9e6a3cd2c2d3527f3ab
Nina MATIC CHARBIT <nina.MATIC-CHARBIT@etu.univ-amu.fr>
2025-11-25 16:49:09 +0100

    Implement voting system and input handling
    Added functions to read strings and integers while ignoring comments. Implemented a two-round voting system for ice cream preferences.

--- vote2tour/vote2tour.cpp
+++ vote2tour/vote2tour.cpp
@@ -1,7 +1,66 @@
-//----Modification----
 /**
- * @date 21/11/2025
+ *  @date : 22 octobre 2024
+ *  @author : Alain Casali et groupe 2 G1
+ *  @Brief : vote 2 tour de la SAE 1.02 de 25/26
+**/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * @brief litUneString
+ * @return la chaine lue sauf si :
+ * (1) on une boulette sur l'entrée
+ * (2) on trouve un commentaire sur l'entrée
+ * le commentaire est matérialisé par la chaine "//"
  */
+
+
+string litUneString (){
+    string uneChaine;
+    while (true){
+        getline (cin, uneChaine);
+        if ((!cin) || (uneChaine.substr(0,2) != "//")) break;
+    }
+    return uneChaine;
+}
+
+
+int litUnEntier (){
+    string uneChaine;
+    while (true){
+        getline (cin, uneChaine);
+        if ((!cin) || (uneChaine.substr(0,2) != "//")) break;
+    }
+    return stoi(uneChaine);
+}
+
+struct participant {
+    string nom;
+    string prenom;
+    int glacePref;
+};
+
+bool compare2part (const participant & p1, const participant & p2){
+    return p1.prenom < p2.prenom;
+}
+
+void affichVectString (const vector<string> & v){
+    for (const string & val : v)
+        cout << val << '\t';
+    cout << endl;
+}
+
+void affichVectParticipants (const vector<participant> & vPart){
+    for (const participant & part : vPart){
+        cout << part.nom << endl;
+        cout << part.prenom << endl;
+        cout << part.glacePref << endl;
+    }
+}
+
 //--------------------------CODE MODIFIE-------------------------------------
 
 // on va detecter les glaces preférées des votants tout en virant les commentaires
@@ -22,20 +81,20 @@ int main()
     for (unsigned i=0; i < 4; i=i+1)
         vGlacePref.push_back(litUneString());
     //affichVectString(vGlacePref);
-
+    
     //On lit les datas du clavier, et on les stocke :
     vector<participant> vParticipant;
     enregistrePart(vParticipant);
-
+    
     //------Vote majoritaire en deux tours :
-
+    
     //Votes du premier tour
     vector<int> vCompteur (4,0); //4 glaces
     for (const participant & participant : vParticipant){ //boucle qui passe pour chaque participants du vecteur
         vCompteur[participant.glacePref-1] += 1; // rajoute 1 pour le num de la glace qui est entre 1 et 4 donc on enleve 1
     }
     //cout << "Compteur Choco : " << vCompteur[0] << "   Compteur pour Fraises : " << vCompteur[1] << "   Compteur pour Banane : " << vCompteur[2] << "   Compteur pour Mangue : " << vCompteur[3] << endl;
-
+    
     //Cas de la majorité absolue qui fait une simple vérification sur la moitié du nb de participants
     bool majoriteAbsolue (false);
     for (unsigned i=0; i < vCompteur.size(); i=i+1){
@@ -44,7 +103,7 @@ int main()
             cout << "la glace élue est : " << vGlacePref[i] << endl;
         }
     }
-
+    
     //Cas sans majorité absolue
     if (!majoriteAbsolue){
         //Les deux glaces en tete seront definies par max1 et max2
@@ -59,19 +118,19 @@ int main()
                 max2 = i;
             }
         }
-
+        
         //On prends les votes du second tour
         vector<participant> vParticipant2;
         enregistrePart(vParticipant2);
-
+        
         //On refait un comptage
         vector<int> vCompteur2 (4,0);
         for (const participant & part : vParticipant2){ //boucle qui passe pour chaque participants du vecteur
             vCompteur2[part.glacePref-1] += 1; // rajoute 1 pour le num de la glace qui est entre 1 et 4 donc on enleve 1
         }
-
+        
         //cout << "Compteur " << vGlacePref[max1] << " : " << vCompteur2[0] << endl << "Compteur " << vGlacePref[max2] << " : " << vCompteur2[1] << endl;
-
+        
         //On annonce le résultat final :
         if (vCompteur2[0] > vCompteur2[1])
             cout << "la glace élue est : " << vGlacePref[max1] << endl;
@@ -79,11 +138,7 @@ int main()
             cout << "la glace élue est : " << vGlacePref[max2] << endl;
         else
             cout << "Egalité entre les deux glaces en tête" << endl;
-
+        
     }
-    return 0;
-}
-
-
     return 0;
 }
